bf3a6b74c3793670b7bd69093e73becb6fa4f7ca
KITAITI Makoto <KitaitiMakoto@gmail.com>
2017-02-12 00:59:04 +0900

    Add some methods to Library

--- lib/pirka/library.rb
+++ lib/pirka/library.rb
@@ -1,15 +1,77 @@
+require "pathname"
 require "base64"
 require "yaml"
+require "epub/parser/cfi"
 
 module Pirka
+  # Environment variables affect this class:
+  #   XDG_DATA_HOME: Directory to store library files and read. Defaults to $HOME/.local/share
+  #   XDG_DATA_DIRS: Directories to search library files. Separated with a colon ':'. Defaults to /usr/local/share:/usr/share
+  # 
+  # @see https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
   class Library
     EXT = ".yaml"
+    XDG_DATA_HOME = ".local/share"
+    XDG_DATA_DIRS = "/usr/local/share:/usr/share"
+
+    class << self
+      attr_accessor :directory
+
+      # @return [Array<Pathname>]
+      def directories
+        return [@directory] if @directory
+
+        [home] +
+          (ENV["XDG_DATA_DIRS"] || XDG_DATA_DIRS).split(":").collect {|dir| Pathname.new(dir)}
+      end
+
+      # @see https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+      def home(user = nil)
+        Pathname.new(ENV["XDG_DATA_HOME"] || Dir.home(user) + XDG_DATA_HOME) + "pirka"
+      end
+
+      # @param [String] release_identifier
+      # @return [Library, nil]
+      def find_by_release_identifier(release_identifier)
+        lib_path = basename
+        directories.each do |dir|
+          path = dir/lib_path
+          return from_file(path) if path.file?
+        end
+        nil
+      end
+
+      # @param [Pathname, String] path
+      # @return [Library]
+      def from_file(path)
+        from_hash(YAML.load_file(path.to_s))
+      end
+
+      # @param [Hash] h
+      # @return [Library]
+      def from_hash(h)
+        library = new
+
+        h.each_pair do |key, value|
+          if key == "codelist"
+            value.each_pair do |cfi, data|
+              library.codelist[EPUB::Parser::CFI.parse(cfi)] = data
+            end
+          else
+            library.metadata[key] = value
+          end
+        end
+
+        library
+      end
+    end
 
     attr_reader :metadata, :codelist
 
-    # @param [Pathname] directory for library files
-    def initialize(directory)
+    # @param [Pathname, nil] directory for library files
+    def initialize(directory: nil, path: nil)
       @directory = directory
+      @path = path
       @metadata = {}
       @codelist = {}
     end
@@ -46,6 +108,7 @@ module Pirka
       end
     end
 
+    # @return [String]
     def to_yaml
       metadata.merge({
         "codelist" => each.with_object({}) {|(cfi, value), list|
