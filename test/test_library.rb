1931947ac454adb946be7b45fd590218b3bffc10
KITAITI Makoto <KitaitiMakoto@gmail.com>
2017-02-11 22:45:27 +0900

    Add test case for Library#each with block

--- test/test_library.rb
+++ test/test_library.rb
@@ -14,6 +14,17 @@ class TestLibrary < Test::Unit::TestCase
   end
 
   def test_each_iterates_over_list_in_order_of_cfi
+    cfis = %w[/6/30!/4/2/56/2
+              /6/30!/4/2/58/2
+              /6/31!/4/2/56/2]
+    i = 0
+    @library.each do |(cfi, _)|
+      assert_equal cfi.to_s, cfis[i]
+      i += 1
+    end
+  end
+
+  def test_each_returns_enumerator
     assert_equal %w[/6/30!/4/2/56/2
                     /6/30!/4/2/58/2
                     /6/31!/4/2/56/2],
