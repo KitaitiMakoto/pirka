3d86334817e1f4b985b724f4d70d0e2b1fe67d2b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2017-03-05 22:29:05 +0900

    Fix namespace of ClassName middleware

--- lib/pirka/highlighter.rb
+++ lib/pirka/highlighter.rb
@@ -26,24 +26,24 @@ module Pirka
           element.inner_html = @formatter.format(lexer.lex(element.content)) # @todo Consider the case `element` has descendants
         end
       end
-    end
 
-    class ClassName
-      ATTR_NAME = "class"
-      ATTR_SEPARATOR = /\s+/
-      CLASS_NAME = "pirka"
+      class ClassName
+        ATTR_NAME = "class"
+        ATTR_SEPARATOR = /\s+/
+        CLASS_NAME = "pirka"
 
-      def initialize(highlighter, class_name: CLASS_NAME)
-        @highlighter = highlighter
-        @class_name = class_name
-      end
+        def initialize(highlighter, class_name: CLASS_NAME)
+          @highlighter = highlighter
+          @class_name = class_name
+        end
 
-      def markup(element, lang)
-        @highlighter.markup element, lang
-        class_names = (element[ATTR_NAME] || "").split(ATTR_SEPARATOR)
-        return class_names.include? @class_name
-        class_names << @class_name
-        element[ATTR_NAME] = class_names.join(" ")
+        def markup(element, lang)
+          @highlighter.markup element, lang
+          class_names = (element[ATTR_NAME] || "").split(ATTR_SEPARATOR)
+          return if class_names.include? @class_name
+          class_names << @class_name
+          element[ATTR_NAME] = class_names.join(" ")
+        end
       end
     end
   end
