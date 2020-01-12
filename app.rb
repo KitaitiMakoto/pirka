1343313158fcd2525c99f8ff79369ad3fdcc1706
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2020-01-12 21:55:50 +0900

    Mark up app.js with gettext

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ gem "epub-parser", ">= #{Pirka::EPUB_PARSER_VERSION}"
 
 module Pirka
   class App
-    DESCRIPTION = "Pirka highlights source code syntax in EPUB files"
+    DESCRIPTION = _("Pirka highlights source code syntax in EPUB files")
     APPS = {}
 
     def initialize
@@ -36,34 +36,34 @@ module Pirka
         opt.version = Pirka::VERSION
 
         opt.banner = <<EOB % {description: DESCRIPTION, program_name: opt.program_name}
-%{description}
+n%{description}
 
 Usage: %{program_name} [global options] [<command>] [options]
 EOB
 
         opt.separator ""
-        opt.separator "Global options:"
-        opt.on "-c", "--config=FILE", "Config file. Defaults to %{config_path}" % {config_path: Config.filepath}, Pathname do |path|
+        opt.separator _("Global options:")
+        opt.on "-c", "--config=FILE", _("Config file. Defaults to %{config_path}") % {config_path: Config.filepath}, Pathname do |path|
           config_path = path
         end
-        opt.on "-s", "--data-home=DIRECTORY", "Directory to *SAVE* library data", Pathname do |path|
+        opt.on "-s", "--data-home=DIRECTORY", _("Directory to *SAVE* library data"), Pathname do |path|
           @tmp_opts["data_home"] = path
         end
-        opt.on "-d", "--directory=DIRECTORY", "Directory to *SEARCH* library data.", "Specify multiple times to add multiple directories.", Pathname do |path|
+        opt.on "-d", "--directory=DIRECTORY", _("Directory to *SEARCH* library data."), _("Specify multiple times to add multiple directories."), Pathname do |path|
           @tmp_opts["additional_directories"] << path
         end
-        opt.on "--debug", "Set debugging flag" do
+        opt.on "--debug", _("Set debugging flag") do
           $DEBUG = true
         end
 
         opt.separator ""
-        opt.separator "Commands:"
+        opt.separator _("Commands:")
         width = APPS.keys.collect(&:length).max
         APPS.each_pair do |command, app|
           opt.separator opt.summary_indent + command.ljust(width) +
                         opt.summary_indent * 2 + app::DESCRIPTION
         end
-        opt.separator "If command is ommitted, highlight is used with no option"
+        opt.separator _("If command is ommitted, highlight is used with no option")
       }
       parser.order! argv
       @config = config_path.file? ? Config.load_file(config_path) : Config.new
