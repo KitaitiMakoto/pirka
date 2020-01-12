d01b736e7e080686af094013e08a6cb71ccb520e
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2020-01-12 14:04:46 +0900

    Make Pirka include GetText

--- lib/pirka.rb
+++ lib/pirka.rb
@@ -1,1 +1,12 @@
 require 'pirka/version'
+require "gettext"
+
+module Pirka
+  include GetText
+
+  bindtextdomain Gem::Specification.load(File.join(__dir__, "../pirka.gemspec"))
+end
+
+require "pirka/library"
+require "pirka/config"
+require "pirka/highlighter"
